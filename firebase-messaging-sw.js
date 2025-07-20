e078200f2ba951a113a79edc03dac18d0258a551
professionalnill <m.nillroy@gmail.com>
2025-07-20 10:50:17 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,22 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyD2si9OGF9frCj2agwBltcCwtm2qWzQrKo",
   authDomain: "notification-fe3d3.firebaseapp.com",
   projectId: "notification-fe3d3",
-  storageBucket: "notification-fe3d3.appspot.com",
+  storageBucket: "notification-fe3d3.firebasestorage.app",
   messagingSenderId: "268120039765",
-  appId: "1:268120039765:web:7866eef2c294d5e5524fdc"
+  appId: "1:268120039765:web:7866eef2c294d5e5524fdc",
+  measurementId: "G-RW5BH1Z47M"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, { body, icon });
-});
-
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
+messaging.onBackgroundMessage(payload => {
+  console.log('[firebase-messaging-sw.js] Received background message:', payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: payload.notification.icon || "https://firebase.google.com/favicon.ico"
+  });
 });
